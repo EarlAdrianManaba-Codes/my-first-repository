dbad411ddf41172402892d83e1bf08b1822042ff
EARL MANABA <earladrianmanaba@gmail.com>
2026-09-20 20:52:24 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-   
-        Movie movie1 = new Movie();
-        movie1.title = "Titanic";     
-        movie1.genre = "Romance/Drama";      
-        movie1.duration = 314;   
 
-        Movie movie2 = new Movie();
-        movie2.title = "Parasite";
-        movie2.genre = "Thriller/Drama";
-        movie2.duration = 212;
+        Vehicle v1 = new Vehicle();
+        v1.brand = "Toyota";
+        v1.model = "Corolla";
+        v1.year = 2018;
 
-        Movie movie3 = new Movie();
-        movie3.title = "Toy Story";
-        movie3.genre = "Animation/Comedy/Adventure";
-        movie3.duration = 121;
+        Vehicle v2 = new Vehicle();
+        v2.brand = "Ford";
+        v2.model = "Mustang";
+        v2.year = 1995;
 
-        movie1.displayInfo();
-        movie2.displayInfo();
-        movie3.displayInfo();
+        Vehicle v3 = new Vehicle();
+        v3.brand = "Tesla";
+        v3.model = "Model 3";
+        v3.year = 2022;
+
+        System.out.println(" Vehicle 1 ");
+        v1.displayInfo();
+        System.out.println("Age: " + v1.calculateAge() + " years");
+        System.out.println("Is Vintage: " + v1.isVintage());
+        System.out.println();
+
+        System.out.println(" Vehicle 2 ");
+        v2.displayInfo();
+        System.out.println("Age: " + v2.calculateAge() + " years");
+        System.out.println("Is Vintage: " + v2.isVintage());
+        System.out.println();
+
+        System.out.println(" Vehicle 3 ");
+        v3.displayInfo();
+        System.out.println("Age: " + v3.calculateAge() + " years");
+        System.out.println("Is Vintage: " + v3.isVintage());
     }
 }
